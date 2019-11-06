d3cd4fdf19e3ae50e4eafda0527294607bab70a2
Wilson Júnior <wilsonpjunior@gmail.com>
2019-11-06 10:05:26 -0300

    Trigger first travis build

--- example.py
+++ example.py
@@ -3,7 +3,6 @@ from aiohttp_prometheus import MetricsMiddleware, MetricsView
 
 app = web.Application()
 app.middlewares.append(MetricsMiddleware())
-
 app.router.add_route('GET', '/metrics', MetricsView)
 
 web.run_app(app)
